2cb4005afe00f0631639bd71f7120557074a7c14
Vladimir <w112vovan@mail.ru>
2023-11-27 14:28:29 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L17_powerOfTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int baseNumber = 2;
+            int powerOfNumber = 2;
+            int finalNumber = baseNumber;
+            int minRangeValue = 4;
+            int maxRangeValue = 101;
+            int targetNumber = random.Next(minRangeValue, maxRangeValue);
+            bool isOpen = true;
+
+            while (isOpen)
+            {
+                finalNumber *= baseNumber;
+
+                if (finalNumber < targetNumber)
+                    powerOfNumber++;
+                else
+                    isOpen = false;
+            }
+            Console.WriteLine($"Исходное число: {targetNumber}.\n{baseNumber} в степени {powerOfNumber} равно {finalNumber}.\n");
+        }
+    }
+}
